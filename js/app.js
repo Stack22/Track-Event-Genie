2bec6faa530669017e5fb97477b4442af347f9a3
Stack22 <stacyking1421@gmail.com>
2017-02-11 12:23:56 -0500

    add main title text, center start box

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-$(document).ready(function() {
-	$.getJSON('https://api.motorsportreg.com/rest/calendars/organization/{your-id-here}.jsonp?jsoncallback=?', {
-			dataType: "jsonp"
-			,cacheBuster: new Date()
-		 }, function(json) {
+// API init/call
+var MSR_API_URL = "https://api.motorsportreg.com/rest/calendars/"
+var avOrgID = '4F1E597B-9955-0AB6-1136213C81AE96EF'
+var query = {
+  username: 'trackpedia.com',
+  password: 'traqw1ki',
+  dataType: 'jsonp',
+  cacheBuster: new Date(),
+  postalcode: 27705,
+};
+// organization/4F1E597B-9955-0AB6-1136213C81AE96EF.jsonp?jsoncallback=?'
+
+function getData() {
+	$.getJSON(MSR_API_URL, query,
+     function(json) {
 			var tbl = '<table>';
 			$.each(json.response.events, function(i, evt) {
 				tbl += '<tr>';
@@ -17,4 +27,8 @@ $(document).ready(function() {
 			tbl += '<' + '/table>';
 			$('#msrCalendar').append(tbl);
 		});
+};
+
+$(function() {
+  getData();
 });
