f375a91d89659931698df9d77ef259660dec22ee
Stack22 <stacyking1421@gmail.com>
2017-02-16 00:47:48 -0500

    add render resultsbox function (not working)

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ function saveResults(data) {
   console.log(state.cleandata);
 };
 
-function sortByVenue(state) {
+function sortByVenue(state, resultsElement) {
   console.log("soryByVenue");
   return state.cleandata.sort(function(a, b) {
     var nameA = a.venue.name.toLowerCase();
@@ -54,19 +54,38 @@ function sortByVenue(state) {
     return 0;
   });
   console.log(state.cleandata);
+  renderResultsBox(state, resultsElement);
 };
 
-function renderLinksHtml(state) { // creates array of html 1st 3 events
-
+function renderLinksHtml(array) { // creates array of html 1st 3 events
   var content = [];
   for (i=0; i<3; i++) {
-    content.push("<p><a href='" + state.cleandata[i].eventurl + "'>" + state.cleandata[i].eventdate + " / " + state.cleandata[i].eventname + "</a></p>");
+    content.push("<p><a href='" + array.cleandata[i].eventurl + "'>" + array.cleandata[i].eventdate + " / " + array.cleandata[i].eventname + "</a></p>");
   };
   console.log(content);
 };
 
+function renderResultsBox(state, resultsElement) {
+  var content = state.cleandata.map(function(event) {
+    return '<div class="resultcard col-6 js-resultcard">' +
+    '<img class="logobox" src="images/AV_Web_-32.jpg" alt="track-logo">' + '<div class="trackinfobox"><span class="trackname">' + event.venue.name + '</span><br><br>' +
+      '<span class="trackcity">' + event.venue.city + ', ' + event.venue.region + '</span><br><br>' +
+      '<span class="maplink"><a href="#">View on Map</a></span>' +
+      '</div><hr>' +
+      '<div class="linksbox js-links-box">' +
+      '<h3>Upcoming events:</h3>' +
+      '<ul class="eventLinks">' +
+      '<li><a href="#">2017-2-25   THSCC HPDE</a></li>' +
+      '<li><a href="#">2017-3-5    Someone Else Track Day</a></li>' +
+      '<li><a href="#">2017-3-12   NCR SCCA Majors</a></li>' +
+      '</ul></div></div>'
+  });
+  resultsElement.removeClass(".hidden");
+  resultsElement.html(content);
+};
+
 // Event listeners
-function watchForSubmit(formElement, zipInputElement, radiusInputElement, submitButton, searchTypeElement) {
+function watchForSubmit(formElement, zipInputElement, radiusInputElement, submitButton, searchTypeElement, resultsElement) {
   submitButton.click(function(e) {
     // resetState(state);
     e.preventDefault();
@@ -75,7 +94,7 @@ function watchForSubmit(formElement, zipInputElement, radiusInputElement, submit
     state.apiQuery.data.radius = $(radiusInputElement).val();
     getData(state, saveResults);
     if (searchTypeElement.val() === "venue") {
-      sortByVenue(state);
+      sortByVenue(state, resultsElement);
     } else if (searchTypeElement.val() === "proximity") {
       //sortByProximity(state);
     } else {
@@ -89,8 +108,9 @@ var radiusInputElement = $(".js-radius-input");
 var formElement = $(".js-form");
 var submitButton = $("#search-button");
 var searchTypeElement = $(".js-search-type");
+var resultsElement = $("#js-resultbox")
 
 $(function() {
-  watchForSubmit(formElement, zipInputElement, radiusInputElement, submitButton, searchTypeElement);
+  watchForSubmit(formElement, zipInputElement, radiusInputElement, submitButton, searchTypeElement, resultsElement);
 
 });
